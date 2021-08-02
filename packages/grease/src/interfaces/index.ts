099a67e1f3e1c77e8795ffa0af292f007415d164
Lex <lex@flexdevelopment.llc>
2021-08-02 14:44:30 -0400

    feat(node-typescript): `interfaces/IsPathOptions`
    P010-6

--- packages/grease/src/interfaces/index.ts
+++ packages/grease/src/interfaces/index.ts
@@ -4,4 +4,5 @@
  */
 
 export type { IsBranchOptions } from './is-branch-options.interface'
+export type { IsPathOptions } from './is-path-options.interface'
 export type { IsTargetBranchOptions } from './is-target-branch-options.interface'
