dddf198a4061579921e26bf137427b2d822521ee
Lex <lex@flexdevelopment.llc>
2021-08-02 11:32:46 -0400

    feat(node-typescript): `enums/IsTargetBranchMessage`
    P010-6

--- packages/grease/src/enums/index.ts
+++ packages/grease/src/enums/index.ts
@@ -6,3 +6,4 @@
 export { IsBranchMessage } from './is-branch-message.enum'
 export { IsCommitMessage } from './is-commit-message.enum'
 export { IsTargetBranchCode } from './is-target-branch-code.enum'
+export { IsTargetBranchMessage } from './is-target-branch-message.enum'
---
+++ packages/grease/src/enums/is-target-branch-message.enum.ts
@@ -0,0 +1,19 @@
+/**
+ * @file Enums - IsTargetBranchMessage
+ * @module grease/enums/IsTargetBranchMessage
+ */
+
+/**
+ * `IsTargetBranch` decorator error codes mapped to error messages.
+ *
+ * Messages may use [special class-validator tokens][1].
+ *
+ * [1]: https://github.com/typestack/class-validator#validation-messages
+ */
+export enum IsTargetBranchMessage {
+  /**
+   * Value is a non-empty string, but is not the name of a remote branch or
+   * commit SHA in the current repository.
+   */
+  DOES_NOT_EXIST = '$property must be name of remote branch or commit sha'
+}
