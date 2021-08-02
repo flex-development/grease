672b8f1abc0cc931388a738ae5612b71c91de5ed
Lex <lex@flexdevelopment.llc>
2021-08-02 16:15:31 -0400

    feat(node-typescript): `interfaces/IsSemVerOptions`
    P010-6

--- packages/grease/src/interfaces/index.ts
+++ packages/grease/src/interfaces/index.ts
@@ -5,4 +5,5 @@
 
 export type { IsBranchOptions } from './is-branch-options.interface'
 export type { IsPathOptions } from './is-path-options.interface'
+export type { IsSemVerOptions } from './is-sem-ver-options.interface'
 export type { IsTargetBranchOptions } from './is-target-branch-options.interface'
---
+++ packages/grease/src/interfaces/is-sem-ver-options.interface.ts
@@ -0,0 +1,51 @@
+import type {
+  GitSemverTagsOptions,
+  IsSemVerOptionsConfigClean,
+  IsSemVerOptionsConfigCMP,
+  IsSemVerOptionsConfigSatisfies
+} from '@grease/types'
+import type { ValidationOptions } from 'class-validator'
+import type { CoerceOptions } from 'semver'
+
+/**
+ * @file Interfaces - IsSemVerOptions
+ * @module grease/interfaces/IsSemVerOptions
+ */
+
+/**
+ * Validation options for the `IsSemVer` decorator.
+ *
+ * @extends ValidationOptions
+ */
+export interface IsSemVerOptions extends ValidationOptions {
+  /**
+   * Remove tag prefix and any leading or trailing whitespaces from $value.
+   */
+  clean?: IsSemVerOptionsConfigClean | boolean
+
+  /**
+   * Compare $value to another semantic version.
+   */
+  cmp?: IsSemVerOptionsConfigCMP | []
+
+  /**
+   * Coerce $value to a semantic version if possible.
+   */
+  coerce?: CoerceOptions | boolean
+
+  /**
+   * Check if $value is a semantic version tag pushed to the current repository.
+   */
+  git?: GitSemverTagsOptions | boolean
+
+  /**
+   * Check if $value is a semantic version tag, but **not** pushed to the
+   * current repository. Use `git` to configure tag search.
+   */
+  negit?: boolean
+
+  /**
+   * Check if $value satisfies specified range.
+   */
+  satisfies?: IsSemVerOptionsConfigSatisfies | []
+}
