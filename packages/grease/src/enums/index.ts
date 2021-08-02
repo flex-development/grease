308b3c9d48da699ad53cc1339bb697ca9b3f052f
Lex <lex@flexdevelopment.llc>
2021-08-02 11:29:53 -0400

    feat(node-typescript): `enums/IsTargetBranchCode`
    P010-6

--- packages/grease/src/enums/index.ts
+++ packages/grease/src/enums/index.ts
@@ -5,3 +5,4 @@
 
 export { IsBranchMessage } from './is-branch-message.enum'
 export { IsCommitMessage } from './is-commit-message.enum'
+export { IsTargetBranchCode } from './is-target-branch-code.enum'
