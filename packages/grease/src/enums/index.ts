b0f77feb29f4870bd47ad86e463161a8b2fc6d69
Lex <lex@flexdevelopment.llc>
2021-08-02 14:41:32 -0400

    feat(node-typescript): `enums/IsPathCode`
    P010-6

--- packages/grease/src/enums/index.ts
+++ packages/grease/src/enums/index.ts
@@ -5,5 +5,6 @@
 
 export { IsBranchMessage } from './is-branch-message.enum'
 export { IsCommitMessage } from './is-commit-message.enum'
+export { IsPathCode } from './is-path-code.enum'
 export { IsTargetBranchCode } from './is-target-branch-code.enum'
 export { IsTargetBranchMessage } from './is-target-branch-message.enum'
