a9585517dc8e9803fb81a2de866eb9d9dc4fcc9b
Lex <lex@flexdevelopment.llc>
2021-08-03 01:23:15 -0400

    feat(node): `utils/createLogger`
    P010-9

---
+++ packages/grease/src/config/constants.ts
@@ -0,0 +1,16 @@
+/**
+ * @file Configuration - Constant Values
+ * @module grease/config/constants
+ */
+
+/**
+ * @property {string} DEBUG_NAMESPACE - Name of `debug` namespace
+ * @see https://github.com/visionmedia/debug
+ */
+export const DEBUG_NAMESPACE = 'grease'
+
+/**
+ * @property {string} DEBUG_NAMESPACE_COLOR - `debug` namespace color
+ * @see https://github.com/visionmedia/debug#namespace-colors
+ */
+export const DEBUG_NAMESPACE_COLOR = '111111111111'
