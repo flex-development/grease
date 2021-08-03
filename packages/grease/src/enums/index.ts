e354a7b5083b22efa3411b118200ce7e6911f725
Lex <lex@flexdevelopment.llc>
2021-08-03 01:30:49 -0400

    feat(node-typescript): `enums/ExitCode`
    P010-10

--- packages/grease/src/enums/index.ts
+++ packages/grease/src/enums/index.ts
@@ -4,6 +4,7 @@
  */
 
 export { DependencyCommand } from './dependency-command.enum'
+export { ExitCode } from './exit-code.enum'
 export { IsBranchMessage } from './is-branch-message.enum'
 export { IsCommitMessage } from './is-commit-message.enum'
 export { IsPathCode } from './is-path-code.enum'
