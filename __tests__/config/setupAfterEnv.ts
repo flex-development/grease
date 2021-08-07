930e9ca0df81be7fc3203f2ab38398dac6e66d42
Lex <lex@flexdevelopment.llc>
2021-08-07 13:58:29 -0400

    chore(deps-dev): install `jest-mock-console@1.1.0`
    P010-17

--- __tests__/config/setupAfterEnv.ts
+++ __tests__/config/setupAfterEnv.ts
@@ -1,7 +1,10 @@
 import 'jest-extended'
+import mockConsole from 'jest-mock-console'
 
 /**
  * @file Test Configuration - Setup After Environment
  * @module tests/config/setupAfterEnv
  * @see https://jestjs.io/docs/configuration#setupfilesafterenv-array
  */
+
+mockConsole()
