cf98b3dea823c773dab8b7b388f35cbe97f2c452
Lex <lex@flexdevelopment.llc>
2021-08-08 01:28:12 -0400

    refactor(node): `lifecycles/greaser`

--- packages/grease/src/lifecycles/__tests__/greaser.lifecycle.functional.spec.ts
+++ packages/grease/src/lifecycles/__tests__/greaser.lifecycle.functional.spec.ts
@@ -1,6 +1,10 @@
 import { GH_RELEASE_CREATE } from '@grease/config/constants.config'
-import { ExitCode } from '@grease/enums/exit-code.enum'
+import logger from '@grease/config/logger.config'
+import type { ICreateReleaseDTO } from '@grease/interfaces'
+import GreaseOptions from '@grease/models/grease-options.model'
+import validate from '@grease/utils/validate.util'
 import sh from 'shelljs'
+import runLifecycleScript from 'standard-version/lib/run-lifecycle-script'
 import TestSubject from '../greaser.lifecycle'
 
 /**
@@ -8,20 +12,66 @@ import TestSubject from '../greaser.lifecycle'
  * @module grease/lifecycles/tests/functional/Greaser
  */
 
+jest.mock('@grease/config/logger.config')
+jest.mock('@grease/utils/validate.util')
+
 const mockSH = sh as jest.Mocked<typeof sh>
+const mockLogger = logger as jest.Mocked<typeof logger>
+const mockRunLifecycleScript = runLifecycleScript as jest.MockedFunction<
+  typeof runLifecycleScript
+>
+const mockValidate = validate as jest.MockedFunction<typeof validate>
 
 describe('functional:lifecycles/greaser', () => {
-  beforeEach(async () => {
-    await TestSubject({ version: '3.13.98-rc.6.40' })
+  const options: GreaseOptions = {}
+  const dto: ICreateReleaseDTO = { version: '3.13.98-rc.6.40' }
+
+  describe('executes lifecycle', () => {
+    beforeEach(async () => {
+      await TestSubject({}, dto)
+    })
+
+    it('should run lifecycle scripts', () => {
+      expect(mockRunLifecycleScript).toBeCalledTimes(2)
+      expect(mockRunLifecycleScript).toBeCalledWith(options, 'pregreaser')
+      expect(mockRunLifecycleScript).toBeCalledWith(options, 'postgreaser')
+    })
+
+    it('should log checkpoints', () => {
+      expect(mockLogger.checkpoint).toBeCalledTimes(2)
+    })
+
+    it('should validate release data', () => {
+      expect(mockValidate).toBeCalledTimes(1)
+    })
+
+    it('should execute github release command', () => {
+      expect(mockSH.exec).toBeCalledTimes(1)
+      expect(mockSH.exec.mock.calls[0][0]).toMatch(GH_RELEASE_CREATE)
+    })
   })
 
-  it(`should execute ${GH_RELEASE_CREATE} command`, () => {
-    expect(mockSH.exec).toBeCalledTimes(1)
-    expect(mockSH.exec.mock.calls[0][0]).toMatch(GH_RELEASE_CREATE)
-  })
+  describe('skips lifecycle', () => {
+    type Case = { lifecycle: 'depchecker' | 'greaser' }
 
-  it('should force shell to exit', () => {
-    expect(mockSH.exit).toBeCalledTimes(1)
-    expect(mockSH.exit).toBeCalledWith(ExitCode.SUCCESS)
+    const cases: Case[] = [
+      { lifecycle: 'depchecker' },
+      { lifecycle: 'greaser' }
+    ]
+
+    const name = 'should skip lifecycle if options.skip.$lifecycle === true'
+
+    it.each<Case>(cases)(name, testcase => {
+      // Arrange
+      const { lifecycle } = testcase
+
+      // Act
+      TestSubject({ skip: { [lifecycle]: true } }, dto)
+
+      // Expect
+      expect(mockRunLifecycleScript).not.toBeCalled()
+      expect(mockLogger.checkpoint).not.toBeCalled()
+      expect(mockSH.which).not.toBeCalled()
+    })
   })
 })
--- packages/grease/src/lifecycles/greaser.lifecycle.ts
+++ packages/grease/src/lifecycles/greaser.lifecycle.ts
@@ -1,10 +1,13 @@
 import type { ObjectPlain } from '@flex-development/tutils'
 import { GH_RELEASE_CREATE } from '@grease/config/constants.config'
+import logger from '@grease/config/logger.config'
 import CreateReleaseDTO from '@grease/dtos/create-release.dto'
-import { ExitCode } from '@grease/enums/exit-code.enum'
 import type { ICreateReleaseDTO as DTO } from '@grease/interfaces'
+import GreaseOptions from '@grease/models/grease-options.model'
 import validate from '@grease/utils/validate.util'
+import ch from 'chalk'
 import sh from 'shelljs'
+import runLifecycleScript from 'standard-version/lib/run-lifecycle-script'
 
 /**
  * @file Lifecycles - Greaser
@@ -17,18 +20,34 @@ import sh from 'shelljs'
  * [1]: https://cli.github.com/manual/gh_release_create
  *
  * @async
+ * @param {GreaseOptions} [options={}] - Application options
  * @param {DTO | ObjectPlain} [dto={}] - Data to create release
- * @return {Promise<never>} Function will force shell to exit
+ * @return {Promise<void>} Empty promise when complete
  */
-const Greaser = async (dto: DTO | ObjectPlain = {}): Promise<never> => {
+const Greaser = async (
+  options: GreaseOptions = {},
+  dto: DTO | ObjectPlain = {}
+): Promise<void> => {
+  // Skip lifecycle
+  if (options.skip?.depchecker || options.skip?.greaser) return
+
+  // Run `pregreaser` script
+  runLifecycleScript(options, 'pregreaser')
+
+  // Log validation checkpoint
+  logger.checkpoint('validating release data...', [], ch.yellow('!!'))
+
   // Validate release data
   const release = await validate(CreateReleaseDTO, dto, false)
+
+  // Log release start checkpoint
+  logger.checkpoint('starting github release...', [], ch.yellow('!!'))
 
   // Execute GitHub release
   sh.exec(`${GH_RELEASE_CREATE} ${release.toString()}`)
 
-  // Force shell to exit
-  return sh.exit(ExitCode.SUCCESS)
+  // Run `postgreaser` script
+  runLifecycleScript(options, 'postgreaser')
 }
 
 export default Greaser
