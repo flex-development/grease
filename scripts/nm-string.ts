710a0e27197c7777a37e246113150e19dc5eaf4c
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-09-15 04:12:27 -0400

    refactor(scripts): simplify `'node_modules'` reference patch logic

--- scripts/nm-string.ts
+++ scripts/nm-string.ts
@@ -10,24 +10,4 @@ import join from 'lodash.join'
  * warning during the `yarn check:install` lifecycle.
  */
 
-/**
- * @property {string} PWD - Root project directory
- */
-const PWD: string = `${process.env.PROJECT_CWD}/`
-
-/**
- * @property {string} ID - Resolve path id as array
- */
-const PARTS: string[] = ['node', '_', 'modules', '/', 'ts-node']
-
-/**
- * @property {string} RESOLVE - Resolved path to `PKG`
- */
-const RESOLVE: string = require.resolve(join(PARTS, ''), { paths: [PWD] })
-
-/**
- * @property {string} NODE_MODULES - 'node_modules'
- */
-const NODE_MODULES: string = RESOLVE.split(PWD)[1].split('/')[0]
-
-export default NODE_MODULES
+export default join(['node', '_', 'modules'], '')
