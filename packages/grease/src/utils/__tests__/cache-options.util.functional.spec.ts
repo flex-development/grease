b8c5c2662f2ee786945bc7c6f113212829c1f4ce
Lexus Drumgold <unicornware@flexdevelopment.llc>
2021-09-18 21:13:24 -0400

    test: use `OPTIONS` fixture in `cacheOptions` functional test

--- packages/grease/src/utils/__tests__/cache-options.util.functional.spec.ts
+++ packages/grease/src/utils/__tests__/cache-options.util.functional.spec.ts
@@ -1,6 +1,5 @@
-import type { IGreaseOptions } from '@grease/interfaces'
 import GreaseOptions from '@grease/models/grease-options.model'
-import { TAGS_OPTIONS_LERNA } from '@grease/tests/fixtures/git-tags.fixture'
+import OPTIONS from '@grease/tests/fixtures/options.fixture'
 import validate from '@grease/utils/validate.util'
 import testSubject from '../cache-options.util'
 
@@ -14,18 +13,12 @@ jest.mock('@grease/utils/validate.util')
 const mockValidate = validate as jest.MockedFunction<typeof validate>
 
 describe('functional:utils/cacheOptions', () => {
-  const options: IGreaseOptions = {
-    lernaPackage: TAGS_OPTIONS_LERNA.package,
-    skipUnstable: TAGS_OPTIONS_LERNA.skipUnstable,
-    tagPrefix: TAGS_OPTIONS_LERNA.tagPrefix
-  }
-
   beforeEach(async () => {
-    await testSubject(options)
+    await testSubject(OPTIONS)
   })
 
   it('should validate application options', () => {
     expect(mockValidate).toBeCalledTimes(1)
-    expect(mockValidate).toBeCalledWith(GreaseOptions, options, false)
+    expect(mockValidate).toBeCalledWith(GreaseOptions, OPTIONS, false)
   })
 })
